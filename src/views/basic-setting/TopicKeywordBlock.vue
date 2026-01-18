ae7565d2ea95300aeda39621b3ffa68dd4ddefb3
norcod <norfecod@outlook.com>
2026-01-18 22:42:06 +0800

    🎈 perf(scripts): 优化“主题关键字屏蔽”选项中关键字长度的限制逻辑

--- src/views/basic-setting/TopicKeywordBlock.vue
+++ src/views/basic-setting/TopicKeywordBlock.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import { ElInput, ElMessage } from 'element-plus';
+import { ElMessage } from 'element-plus';
 
 import { t } from '@/i18n';
 import { BLOCK_KEYWORD_MAX } from '@/constants';
@@ -10,29 +9,14 @@ import type { SettingProps } from '@/types';
 
 const props = defineProps<SettingProps<OptionsKey.TopicKeywordBlock>>();
 
-const inputRef = ref<InstanceType<typeof ElInput> | null>(null);
-
-const isKeywordLengthExceeded = (text: string): boolean => {
-  return text.split('\n').some((line) => line.length > BLOCK_KEYWORD_MAX);
-};
-
 const handleInput = (value: string) => {
-  if (isKeywordLengthExceeded(value)) {
+  const isKeywordInvalid = value.split('\n').some((k) => k.length > BLOCK_KEYWORD_MAX);
+
+  if (isKeywordInvalid) {
     ElMessage.error({
       message: t('keywordBlock.tooLong', { max: BLOCK_KEYWORD_MAX }),
       grouping: true,
     });
-
-    const textarea = inputRef.value?.textarea;
-    const cursorPos = textarea?.selectionStart ?? 0;
-
-    // 恢复光标位置
-    setTimeout(() => {
-      if (textarea && cursorPos) {
-        textarea.selectionStart = cursorPos - 1;
-        textarea.selectionEnd = cursorPos - 1;
-      }
-    }, 0);
   } else {
     props.settings.keywords = value;
   }
@@ -41,7 +25,6 @@ const handleInput = (value: string) => {
 
 <template>
   <ElInput
-    ref="inputRef"
     :model-value="settings.keywords"
     class="topic-keyword-block-textarea"
     type="textarea"
