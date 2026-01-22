3eee96041ad4849d0cf111c2695dda34c7b1e9b6
ccnnde <norfecod@outlook.com>
2026-01-22 20:12:45 +0800

    🎈 perf(scripts): 优化“主题关键字屏蔽”选项组件
    1. 添加帮助按钮
    2. 优化滚动条样式
    3. 禁止缩放文本框

--- src/views/basic-setting/TopicKeywordBlock.vue
+++ src/views/basic-setting/TopicKeywordBlock.vue
@@ -24,25 +24,38 @@ const handleInput = (value: string) => {
 </script>
 
 <template>
-  <ElInput
-    :model-value="settings.keywords"
-    class="topic-keyword-block-textarea"
-    type="textarea"
-    :placeholder="$t('basicSetting.topicKeywordBlock.placeholder')"
-    :rows="6"
-    spellcheck="false"
-    @input="handleInput"
-  />
+  <div class="topic-keyword-block-container">
+    <ElInput
+      :model-value="settings.keywords"
+      type="textarea"
+      :placeholder="$t('basicSetting.topicKeywordBlock.placeholder')"
+      :rows="6"
+      spellcheck="false"
+      resize="none"
+      @input="handleInput"
+    />
+    <ElButton class="topic-keyword-block-help" type="warning" size="small" circle>
+      <un-i-mdi-help />
+    </ElButton>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-.topic-keyword-block-textarea {
+.topic-keyword-block-container {
+  position: relative;
   max-width: 620px;
 
   :deep(.el-textarea__inner) {
     overflow: auto;
     word-wrap: normal;
     white-space: pre;
+    scrollbar-color: rgb(144 147 153 / 30%) transparent;
   }
+}
+
+.topic-keyword-block-help {
+  position: absolute;
+  right: 12px;
+  bottom: 12px;
 }
 </style>
