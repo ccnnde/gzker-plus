fd942f634bb23caf8c7527c4b033ebc548ddef64
norcod <norfecod@outlook.com>
2026-09-06 21:47:29 +0800

    🎈 perf(views): 将主题分享移至侧边操作栏

--- src/components/TopicFooter.vue
+++ src/components/TopicFooter.vue
@@ -1,11 +1,7 @@
 <script setup lang="ts">
 import { computed, inject, onBeforeUnmount, ref, watch } from 'vue';
-import { ElMessage } from 'element-plus';
-import QrcodeVue from 'qrcode.vue';
 
-import { useDarkMode } from '@/composables/dark-mode';
-import { t } from '@/i18n';
-import { addUnit, getTopicUrl } from '@/utils';
+import { addUnit } from '@/utils';
 import { ADD_REPLY_INJECTION_KEY } from '@/constants/inject-key';
 
 import LikeButton from './LikeButton.vue';
@@ -14,8 +10,6 @@ import OperateButton from './OperateButton.vue';
 import type { CSSProperties } from 'vue';
 
 interface Props {
-  topicId?: string;
-  topicTitle?: string;
   replyTotal?: string;
   favorited?: boolean;
   favoriteNumber?: string;
@@ -44,26 +38,6 @@ const TOOLTIP_ENABLE_DELAY = 500;
 const actionTooltipDisabled = ref(false);
 
 let enableTooltipTimer: number | undefined;
-
-const { isDark } = useDarkMode();
-
-const qrcodeStyle = computed(() => {
-  if (isDark.value) {
-    return {
-      background: '#000',
-      foreground: '#fff',
-    };
-  }
-
-  return {
-    background: '#fff',
-    foreground: '#000',
-  };
-});
-
-const topicUrl = computed(() => {
-  return getTopicUrl(props.topicId);
-});
 
 const favoriteIconClass = computed(() => {
   return props.favorited ? 'i-mdi-bookmark' : 'i-mdi-bookmark-outline';
@@ -122,19 +96,6 @@ const replyOrderIconStyle = computed<CSSProperties>(() => {
     fontSize,
   };
 });
-
-const copyTopicUrl = async () => {
-  await navigator.clipboard.writeText(topicUrl.value);
-  ElMessage.success(t('enhancedTopic.copyLinkSuccessfully'));
-};
-
-const shareToWeibo = () => {
-  window.open(
-    `http://service.weibo.com/share/share.php?url=${topicUrl.value}&title=过早客 - ${props.topicTitle}`,
-    '_blank',
-    'width=550, height=370',
-  );
-};
 
 watch(
   () => props.loading,
@@ -199,52 +160,11 @@ const addReply = inject(ADD_REPLY_INJECTION_KEY);
       :tip-disabled="actionTooltipDisabled"
       @click="$emit('toggleReplyOrder')"
     />
-    <ElDropdown trigger="click">
-      <span>
-        <OperateButton :tip-content="$t('enhancedTopic.share')" icon-class="i-mdi-share-variant-outline" />
-      </span>
-      <template #dropdown>
-        <ElDropdownMenu>
-          <ElDropdownItem @click="copyTopicUrl">
-            <un-i-mdi-link-variant class="share-icon share-icon-link" />
-            {{ $t('enhancedTopic.shareLink') }}
-          </ElDropdownItem>
-          <ElDropdownItem @click="shareToWeibo">
-            <un-i-mdi-sina-weibo class="share-icon share-icon-weibo" />
-            {{ $t('enhancedTopic.shareWeibo') }}
-          </ElDropdownItem>
-          <ElDropdownItem class="share-dropdown-wechat">
-            <div>
-              <un-i-mdi-wechat class="share-icon share-icon-wechat" />
-              {{ $t('enhancedTopic.shareWeChat') }}
-            </div>
-            <QrcodeVue
-              :value="topicUrl"
-              :size="65"
-              :background="qrcodeStyle.background"
-              :foreground="qrcodeStyle.foreground"
-            />
-          </ElDropdownItem>
-        </ElDropdownMenu>
-      </template>
-    </ElDropdown>
     <OperateButton v-if="editable" :operate-text="$t('enhancedTopic.editTopic')" @click="$emit('editTopic')" />
     <OperateButton v-else :operate-text="$t('enhancedTopic.blockTopic')" @click="$emit('blockTopic')" />
     <ElInput class="reply-input" :placeholder="$t('enhancedTopic.writeReply')" @focus="addReply?.()" />
   </div>
 </template>
-
-<style lang="scss">
-.share-dropdown-wechat {
-  flex-direction: column;
-
-  & > div {
-    display: flex;
-    align-items: center;
-    margin-bottom: 5px;
-  }
-}
-</style>
 
 <style lang="scss" scoped>
 .footer-container {
@@ -254,23 +174,6 @@ const addReply = inject(ADD_REPLY_INJECTION_KEY);
   border-top: 1px solid var(--el-border-color-lighter);
   border-bottom-right-radius: var(--el-border-radius-base);
   border-bottom-left-radius: var(--el-border-radius-base);
-}
-
-.share-icon {
-  margin-right: 5px;
-  font-size: 16px;
-}
-
-.share-icon-link {
-  color: #9fadc7;
-}
-
-.share-icon-weibo {
-  color: #f46623;
-}
-
-.share-icon-wechat {
-  color: #3fc15f;
 }
 
 .reply-input {
